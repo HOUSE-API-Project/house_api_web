1f969bf7c1ab71aaee6ee0f59d01abb36a1927af
ayafuji <ayafuji.aya@gmail.com>
2013-12-03 14:43:04 +0900

    chage nav

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
         <tr>
 	  <!-- <td class="nav_table_inline">House API Project</td>
 	  <td class="nav_table_inline"></td> -->
-	  <td  class="nav_table_inline">Social Network Service</td>
+	  <td  class="nav_table_inline">SNS Button</td>
 	  <td  class="nav_table_inline"><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://house-api-project.org/" data-text="House API Project">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script><div class="fb-like" data-href="http://house-api-project.org/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div></td>
         </tr>
