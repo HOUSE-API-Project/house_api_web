40b0866bce487b4bdcfab190406b85599e6dc0aa
ayafuji / Yuki Takada <ayafuji.aya@gmail.com>
2013-12-04 00:09:43 +0900

    add hatena bookmark button

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://house-api-project.org/" data-via="House_API">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 <div class="fb-like" data-href="http://house-api-project.org/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div></td>
+        <td><a href="http://b.hatena.ne.jp/entry/http://house-api-project.org/" class="hatena-bookmark-button" data-hatena-bookmark-layout="standard-balloon" data-hatena-bookmark-lang="en" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script></td>
         </tr>
         <tr class="nav_table_inline">
           <td class="nav_table_inline"><a target="_blank" href="http://atnd.org/users/161063">Hackthon</a></td>
